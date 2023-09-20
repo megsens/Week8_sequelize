cb58f155a1db4e70842bdbc5e7bb0d90a9bec171
Megan Farrington <mlfwarr@gmail.com>
2023-09-20 11:14:41 +0100

    Added health route

--- src/server.js
+++ src/server.js
@@ -8,11 +8,23 @@ const express = require("express");
 
 const port = 80;
 
-// initializes port 80
+// initializes port 80 = default port
 
 const app = express();
 
 app.use(express.json());
+
+// "app" = loaded express into APP, to access the ".GET" method (or "GET Request"), the ".GET" method corresponds to a HTTP Verb
+
+// "/health" = connects to the health URL to which the request will be going to i.e: http://localhost/health
+
+// req = request, res = response
+
+// "res.status(200)" = send this message to client, Thunder Client will receive this message
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "API is healthy"});
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
